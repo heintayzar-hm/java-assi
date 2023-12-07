eae69bb3a5c06459dd79df61083c554e080ed00a
Hein Tay Zar <heintayzar@villagelink.co>
2023-12-07 23:53:21 +0630

    add doc in pdf

--- AdministratorInterface.java
+++ AdministratorInterface.java
@@ -1,3 +1,31 @@
+// ## Usage
+
+// You have to run the project depending on the editor you use.
+
+// - All the students will be written inside the students.txt file and you might need to give write access depending on your os.
+// - You can see the view:
+
+// - as we can see, you can add, edit, and view students.
+// - We have predefined all the necessary validations like name length, and age. But you can also change them inside the class of the file, "ValidationUtils", you can see some variables and they are self-explanatory.
+// - If your terminal supports ASCII, you will be able to see color depending on your type.
+
+
+// ## Code Usage
+
+// - We have four classes in this project
+
+// - Student Model class, responsible for a model of a student, you can define static classes for getting a student information or finding a student
+// - ValidationUtils class, responsible for type checking and user validation inputs, the static methods inside responsibility is type checking or validation, if fails, the methods will return static variable Error code.
+// - StudentManagement class, responsible for file loading and saving data updating data, or viewing data that will link with the main class, AdministratorInterface. It will load the file when starting the application using static block and save the data after every data change like update, or add.
+// - AdministratorInterface class, responsible for main user interface, in some ways middleware and route combination in MVC pattern. It will print the user console with available options, and routes. after the user chooses it will link to static methods inside the class. The methods are responsible for getting necessary data and validating them using validation class, a middleware, and linking to the correct method in StudentManagement, a controller.
+
+// So you can think like this in the MVC pattern,
+// AdministratorInterface class: middleware and routing
+// StudentManagement class: controller
+// Student Model class: model
+// ValidationUtils class: a helper middleware class
+
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
